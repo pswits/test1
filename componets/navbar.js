a1339e672bb4717d410af2eb433bf84e9ce36286
pswits <mythpatsahare@gmail.com>
2023-05-30 03:01:57 +0530

    main

--- componets/navbar.js
+++ componets/navbar.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import '../app/globals.css';
 
 function NavLink({ to, children }) {
@@ -7,51 +7,58 @@ function NavLink({ to, children }) {
   </a>;
 }
 
-function MobileNav({ open, setOpen }) {
-  return (
-    <div className={`absolute top-0 left-0 h-screen w-screen bg-black transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
-      <div className="flex items-center justify-center filter drop-shadow-md bg-black h-20"> {/*logo container*/}
-        <a className="text-xl font-semibold text-white" href="/">Reteriors</a>
-      </div>
-      <div className="flex flex-col ml-4">
-        <a className="text-xl font-medium my-4 text-white" href="/about" onClick={() => setTimeout(() => { setOpen(!open) }, 100)}>
-          About
-        </a>
-        <a className="text-xl font-normal my-4 text-white" href="/contact" onClick={() => setTimeout(() => { setOpen(!open) }, 100)}>
-          Contact
-        </a>
-      </div>
-    </div>
-  );
-}
-
 export default function Navbar() {
-
+  const [isMobile, setIsMobile] = useState(false);
   const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  if (isMobile) {
+    return (
+      <nav className="flex filter drop-shadow-md bg-black px-4 py-4 h-20 items-center">
+        <div className="w-3/12 flex items-center">
+          <a className="text-2xl font-semibold text-white" href="/">Reteriors</a>
+        </div>
+        <div className="w-9/12 flex justify-end items-center">
+          <div className="z-50 flex relative w-8 h-8 flex-col justify-between items-center md:hidden" onClick={() => setOpen(!open)}>
+            <span className={`h-1 w-full bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-3.5" : ""}`} />
+            <span className={`h-1 w-full bg-white rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"}`} />
+            <span className={`h-1 w-full bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-3.5" : ""}`} />
+          </div>
+          {open && (
+            <div className="absolute top-0 right-0 h-screen w-64 bg-black transform translate-x-full transition-transform duration-300 ease-in-out filter drop-shadow-md">
+              <div className="flex flex-col ml-4">
+                <NavLink to="/about">ABOUT</NavLink>
+                <NavLink to="/contact">CONTACT</NavLink>
+              </div>
+            </div>
+          )}
+        </div>
+      </nav>
+    );
+  }
+
   return (
     <nav className="flex filter drop-shadow-md bg-black px-4 py-4 h-20 items-center">
-      <MobileNav open={open} setOpen={setOpen} />
       <div className="w-3/12 flex items-center">
         <a className="text-2xl font-semibold text-white" href="/">Reteriors</a>
       </div>
       <div className="w-9/12 flex justify-end items-center">
-
-        <div className="z-50 flex relative w-8 h-8 flex-col justify-between items-center md:hidden" onClick={() => {
-          setOpen(!open);
-        }}>
-          {/* hamburger button */}
-          <span className={`h-1 w-full bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-3.5" : ""}`} />
-          <span className={`h-1 w-full bg-white rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"}`} />
-          <span className={`h-1 w-full bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-3.5" : ""}`} />
-        </div>
-
         <div className="hidden md:flex">
-          <NavLink to="/contact">
-            CONTACT
-          </NavLink>
-          <NavLink to="/about">
-            ABOUT
-          </NavLink>
+          <NavLink to="/about">ABOUT</NavLink>
+          <NavLink to="/contact">CONTACT</NavLink>
         </div>
       </div>
     </nav>
